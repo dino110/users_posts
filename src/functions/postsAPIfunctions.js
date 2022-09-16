567fb7e56c1f581eb6d67288890d71173893cd8f
Dino Kotaranin <dino.kotaranin@gmail.com>
2022-09-16 09:49:04 +0200

    try - catch added

--- src/functions/postsAPIfunctions.js
+++ src/functions/postsAPIfunctions.js
@@ -1,18 +1,33 @@
 import axios from "axios";
 
 export const getPosts = async () => {
-  let posts = await axios.get("https://jsonplaceholder.typicode.com/posts");
-  return posts.data;
+  try {
+    let posts = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    return posts.data;
+  } catch (e) {
+    console.log("error:", e);
+    return [];
+  }
 };
 
 export const getUsers = async () => {
-  let users = await axios.get("https://jsonplaceholder.typicode.com/users");
-  return users.data;
+  try {
+    let users = await axios.get("https://jsonplaceholder.typicode.com/users");
+    return users.data;
+  } catch (e) {
+    console.log("error:", e);
+    return [];
+  }
 };
 
 export const getComments = async () => {
-  let comments = await axios.get(
-    "https://jsonplaceholder.typicode.com/comments"
-  );
-  return comments.data;
+  try {
+    let comments = await axios.get(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+    return comments.data;
+  } catch (e) {
+    console.log("error:", e);
+    return [];
+  }
 };
